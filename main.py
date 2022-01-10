f73471ccf76ac732d17bec8e897c730800f86dca
Fredohm <fredwaucquez@hotmail.fr>
2022-01-10 17:26:19 +0100

    Final commit

--- main.py
+++ main.py
@@ -2,5 +2,21 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 
-
+print("Welcome to the worst coffee machine!\n")
+is_on = True
+while is_on:
+    user_choice = input(f"What would you like? ({menu.get_items()}): ")
+    if user_choice == "off":
+        is_on = False
+    elif user_choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        if menu.find_drink(user_choice):
+            drink = menu.find_drink(user_choice)
+            if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
